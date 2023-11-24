d72d863089e11ac25dff44f5712df3a20d3f834a
Rajat Gupta <138861610+myCode2206@users.noreply.github.com>
2023-11-24 11:24:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: false }));
 
 mongoose
   .connect(
-    "mongodb+srv://shapeShiftDB:mukulbhai16@cluster0.vmp43iw.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://shapeShiftDB:mukulbhai16@cluster0.vmp43iw.mongodb.net/Shapeshiftdb"
   )
   .then(() => {
     console.log("mongoose connected");
